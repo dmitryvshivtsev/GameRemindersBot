f5dfee742e01d1ee748635a76e795d6fbef631b8
dmitryvshivtsev <dmitryvshivtsev@gmail.com>
2023-04-13 20:13:51 +0300

    appended types for parameters

--- bot/database/db_connection.py
+++ bot/database/db_connection.py
@@ -18,26 +18,26 @@ class Database:
         except:
             print('Cant establish connection to database')
 
-    def add_user(self, tg_id, tg_username):
+    def add_user(self, tg_id: int, tg_username: str):
         with self.connection:
             return self.cursor.execute("INSERT INTO users (tg_id, tg_username) VALUES (%s, %s);", (tg_id, tg_username,))
 
-    async def user_exists(self, tg_id):
+    async def user_exists(self, tg_id: int):
         with self.connection:
             self.cursor.execute("SELECT * FROM users WHERE tg_id = %s;", (tg_id,))
             result = len(self.cursor.fetchall())
             return bool(result)
 
-    async def set_favourite_team(self, tg_id, favourite_team):
+    async def set_favourite_team(self, tg_id: int, favourite_team: str):
         with self.connection:
             return self.cursor.execute("UPDATE users SET team_id = (SELECT id FROM teams WHERE team = %s) "
                                        "WHERE tg_id = %s;", (favourite_team, tg_id,))
 
-    async def get_favourite_team(self, tg_id):
+    async def get_favourite_team(self, tg_id: int):
         with self.connection:
             return self.cursor.execute("SELECT team FROM teams WHERE tg_id = %s;", (tg_id,))
 
-    async def get_user_id(self, tg_username):
+    async def get_user_id(self, tg_username: str):
         with self.connection:
             self.cursor.execute("SELECT tg_id FROM users WHERE tg_username = %s;", (tg_username,))
             result = self.cursor.fetchall()
@@ -45,7 +45,7 @@ class Database:
                 userid = int(row(0))
             return userid
 
-    async def get_types(self):
+    async def get_all_types(self):
         with self.connection:
             self.cursor.execute("SELECT DISTINCT kind_of_sport FROM teams ORDER BY kind_of_sport;")
             result = []
@@ -53,7 +53,7 @@ class Database:
             [result.append(*res) for res in query]
             return result
 
-    async def get_leagues(self, kind):
+    async def get_all_leagues(self, kind: str):
         with self.connection:
             self.cursor.execute("SELECT DISTINCT league FROM teams WHERE kind_of_sport = %s ORDER BY league;", (kind,))
             query = self.cursor.fetchall()
@@ -61,7 +61,7 @@ class Database:
             [result.append(*res) for res in query]
             return result
 
-    async def get_teams(self, league):
+    async def get_all_teams(self, league: str):
         with self.connection:
             self.cursor.execute("SELECT DISTINCT team FROM teams WHERE league = %s ORDER BY team;", (league,))
             result = []
@@ -69,7 +69,7 @@ class Database:
             [result.append(*res) for res in query]
             return result
 
-    def get_tag(self, tg_id):
+    def get_all_tags(self, tg_id: int):
         with self.connection:
             self.cursor.execute("SELECT DISTINCT team, team_tag FROM teams WHERE id = (SELECT team_id FROM users WHERE tg_id = %s);", (tg_id,))
             result = []
@@ -77,7 +77,7 @@ class Database:
             [result.append(i) for res in query for i in res]
             return result
 
-    def get_tg_id(self):
+    def get_all_tg_id(self):
         with self.connection:
             self.cursor.execute("SELECT tg_id FROM users;")
             result = []
