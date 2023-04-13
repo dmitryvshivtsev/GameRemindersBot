f5b2fe6406b6e89bb17b5ce2cdab37ad7067eeb3
dmitryvshivtsev <dmitryvshivtsev@gmail.com>
2023-04-13 20:20:42 +0300

    nothing important

--- bot/commands/last_match.py
+++ bot/commands/last_match.py
@@ -11,8 +11,4 @@ from parsing.parsing import send_date_of_match
 
 async def last_match() -> None:
     db = Database()
-    club, team_tag = db.get_tag(message.from_user.id)
-    try:
-        await message.answer(send_date_of_match(club, team_tag))
-    except:
-        await message.answer('К сожалению, на ближайшие дни матчей нет. Если появятся, то я тебе сообщу ;)')
+    pass
